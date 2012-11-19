e7f4987f65ac53e7efdb4317a8eb10bcfacc7c70
JanBrennen <jan@janpaal.com>
2012-11-18 23:08:11 -0600

    code touchups

--- src/false/false.c
+++ src/false/false.c
@@ -2,14 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
 	int i = 1;
-	for(i=1; i < argc; i++){
-	if( !strncmp(argv[1], "--", 2) && !strcasecmp(argv[1], "--version")) 
-	{
-		 puts("Gentils false v0.1"); 
+	for(i = 1; i < argc; i++) {
+		if(!strncmp(argv[1], "--", 2) && !strcasecmp(argv[1], "--version")) {
+			 puts("Gentils false v0.1"); 
+		}
 	}
-}
 	return 1;
 }
