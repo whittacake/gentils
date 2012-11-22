a84446d94481e4673886c01058162c429f948b69
Rafael Khan <raf.khan.f@gmail.com>
2012-11-22 15:23:15 -0500

    ls can actually do it's job now (y)

--- src/ls/ls.c
+++ src/ls/ls.c
@@ -7,11 +7,12 @@
 
 int main(int argc, char** argv) {
 
+	//Points to the locations of directories in argv
 	char** dl = malloc(argc * sizeof(char**));
-	struct arg_list args = {0, 0};
+	struct arg_list args = {0, NULL};
 
 	int i;
-	int j = 0; 
+	int j = 0; //Counts location in dir list
 	for(i = 1; i < argc; i++) {
 		//Check for flag
 		if(argv[i][0] == '-') {
@@ -20,6 +21,7 @@ int main(int argc, char** argv) {
 			//Skip "-" and move to flag content
 			for(n = 1; n < slen; n++) {
 				switch(argv[i][n]) {
+					// -a: Show .*
 					case 'a':
 						args.show_hidden = 1;
 				}
@@ -32,12 +34,14 @@ int main(int argc, char** argv) {
 	dl[j] = NULL;
 	args.dir_list = dl;
 
+	/*
 	for(i = 0; i < (argc - 1); i++) {
 		char* str = args.dir_list[i];
 		if(str != NULL) {
 			printf("%s\n", str);
 		}
 	}
+	*/
 
 	itr_ent(argv[1], args);
 }
@@ -48,23 +52,32 @@ int main(int argc, char** argv) {
 void itr_ent(char* dir_name, struct arg_list args) {
 	struct dirent **eps;
 
-	int d;
-	if(args.show_hidden == 1) {
-		d  = scandir("./", &eps, one, alphasort);
-	} else {
-		d = scandir("./", &eps, no_dot, alphasort);
-	}
-
-	if(d >= 0) {
-		int n;
-
-		for(n = 0; n < d; n++) {
-			printf("%s  ", eps[n]->d_name);
+	// Iterate over each directory in arg_list
+	int i = 0;
+	while(1) {
+		char* dir = args.dir_list[i];
+		if(dir == NULL) {
+			break;
 		}
-		printf("\n");
-	} else {
-		printf("ls: cannot access %s: No such file or directory",
-		        dir_name);
+		int d;
+		if(args.show_hidden == 1) {
+			d  = scandir(dir, &eps, one, alphasort);
+		} else {
+			d = scandir(dir, &eps, no_dot, alphasort);
+		}
+		
+		if(d >= 0) {
+			int n;
+			for(n = 0; n < d; n++) {
+				printf("%s  ", eps[n]->d_name);
+			}
+			printf("\n");
+		} else {
+			printf("ls: cannot access %s: No such file or directory",
+							dir_name);
+		}
+
+		i++;
 	}
 }
 
