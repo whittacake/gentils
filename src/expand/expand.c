9fc29687f2a9b0e88a908dff356363f76f2d5bea
trvra <trevor.s.adams@gmail.com>
2012-11-24 02:09:19 -0800

    coded up expand.c, need to test more

--- src/expand/expand.c
+++ src/expand/expand.c
@@ -1,55 +1,36 @@
-/* 
->Actually writing code
->2012
->trvra wrote this, based on Shiggerino's baller echo code
->with some of that sweet, sweet tee.c baked in
-
-Insert license information here at some point 
-*/
-
-/* 
-Usage: expand [-t tab1,tab2,...,tabn] [file ...]
-*/
-
 #include <stdio.h>
-#include <string.h>
-#define space++ ((space)>(8)?(0):(space += 1))
 
 int
 main (int argc, char *argv[])
 {
-	int i;
+	int i, j, space;
 	char c;
-	FILE **g_files    = NULL;
-	char **g_fileName = NULL;
-	int    g_numFiles = 0;
+	FILE* gFile;
+	
+	for (i = 1; i < argc; i++){
+		gFile = fopen(argv[i], "r");
 
-	if (argc < 2)
-		return 0;
+		if(gFile)
+		{
+			space = 0;
+			c = fgetc (gFile);
+			do{
+    			if(c == '\t'){ 
+    				for(j = 8 - space - 1; j >= 0; j--)
+    					printf(" ");
+    			}
+    			else{
+    				if(space == 8)
+    					space = 0;
+    				else
+    					space++;
+    				printf("%c", c);
+    			}
 
-	g_files = (FILE**)malloc(sizeof(FILE**) * g_numFiles);
-
-	i = argc - 1;
-	while( i > 0){
-    	g_files[i] = fopen(g_fileName[i], g_writeMode);
-    		if (!g_files[i]) {
-        		fprintf(stderr, "failed to open %s\n", g_fileName[i]);
-        		return 1;
-    		}
-    }
-
-	/* When I want to solve for -t, it'll go here~~
-	c = getopt(argc, argv, "t");
-
-	if (c != 't') {
-		for (i = 1; i < argc; i++) 
-			printf("%s ", argv[i]);
-		printf("\n");
-	} else {
-	*/
-		for (i = 2; i < argc; i++) 
-			printf("%s ", argv[i]);
-	//}
+    			c = fgetc (gFile);
+   			} while(c != EOF);
+		}
+	}
 
 	return 0;
 }
