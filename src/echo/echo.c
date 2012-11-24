9df72b427fcf7e6cdc67a64f2cee2dfddbfdcfde
nebukadnezzar <nil094@yahoo.com>
2012-11-24 08:02:35 +0000

    rewrite of echo.c, because... how can you possibly fuck that one up this badly

--- src/echo/echo.c
+++ src/echo/echo.c
@@ -1,38 +1,43 @@
-/* 
->Copyright
->2012
-Shiggerino
+/*
+a really simple echo implementation, done by yours truly, nebukadnezzar.
 
-Insert license information here at some point 
+Copyright of previous author applies, I suppose.
+If no license defined, read on:
+
+I, the copyright holder of this work, hereby release it into the public domain.
+This applies worldwide. If this is not legally possible:
+I grant any entity the right to use this work for any purpose, without any conditions,
+unless such conditions are required by law. If law requires a definition of a software
+license, the terms of the MIT/X11 license shall apply.
 */
 
-/* 
-Usage: echo [-n] [string ...] 
+/*
+Usage: echo [-n] [string ...]
 */
 
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 
-int
-main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
+    int i;
+    int omit_nl = false;
 
-	if (argc < 2)
-		return 0;
-
-	int i;
-	char c;
-
-	c = getopt(argc, argv, "n");
-
-	if (c != 'n') {
-		for (i = 1; i < argc; i++) 
-			printf("%s ", argv[i]);
-		printf("\n");
-	} else {
-		for (i = 2; i < argc; i++) 
-			printf("%s ", argv[i]);
-	}
-
-	return 0;
+    for(i=1; i<argc; i++)
+    {
+        /* not using getopt(), because it's shite anyway
+           there's likely a better way to do it, but it works just fine */
+        if((strlen(argv[i]) > 1) && argv[i][0] == '-' && argv[i][1] == 'n')
+        {
+            omit_nl = true;
+            continue;
+        }
+        printf("%s%s", argv[i], (i+1==argc ? "" : " "));
+    }
+    if(!omit_nl)
+    {
+        printf("\n");
+    }
+    return 0;
 }
