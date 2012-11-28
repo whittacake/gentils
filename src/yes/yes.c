dda48f9259835b683861a66125b36372f804ea94
neeee <ne@tewi>
2012-11-28 01:55:52 +0100

    Make yes.c easier on the eyes

--- src/yes/yes.c
+++ src/yes/yes.c
@@ -1,29 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-#define VERSION "Gentils Yes v0.0.1\n"
 
-int main(int argc, char *argv[]){
-  if(argv[1] != NULL)
-    {
-      if(!strcmp(argv[1], "--version"))
-	{
-	  printf(VERSION);
-	  return 0;
-	}
+int main(int argc, char *argv[])
+{
+	char *s;
+	s = argc <2 ? "y" : argv[1];
 
-      while(1)
-	{
-	  printf("%s\n", argv[1]);
-	}
-    }
-
-  else
-    {
 	while(1)
-	  {
-	    printf("y\n");
-	  }
-
-	return 0;
-    }
+		printf("%s\n", s);
 }
